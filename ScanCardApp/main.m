07f7da2d8641c385fa0e208a7dae5884fec4c8f9
Ratheesh TR <Pickzy-Mac-mini-9@mac-mini.local>
2016-10-26 19:58:47 +0530

    Initial Commit

---
+++ ScanCardApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ScanCardApp
+//
+//  Created by Ratheesh TR on 26/10/16.
+//  Copyright © 2016 Ratheesh TR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
